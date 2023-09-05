f11ed299855c99e504bdb8c74dd042763a922f15
mozgosup <mozgosup@gmail.com>
2023-09-05 16:29:17 +0700

    Added the ability to preview a painting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-<script src="assets/js/main.js"></script>
+function openModal(src) {
+    const modal = document.getElementById('imageModal');
+    const modalImage = document.getElementById('img01');
+    modalImage.src = src;
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    const modal = document.getElementById('imageModal');
+    modal.style.display = "none";
+}
