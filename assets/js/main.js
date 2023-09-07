041f14f7f5ffaabb03d7512c91b1a1f65e9f6968
mozgosup <mozgosup@gmail.com>
2023-09-07 17:08:37 +0700

    Adding new paintings has been moved to a separate file

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,41 @@ function closeModal() {
     const modal = document.getElementById('imageModal');
     modal.style.display = "none";
 }
+
+function populateGallery(paintings) {
+    const gallery = document.getElementById('gallery');
+
+    paintings.forEach((painting) => {
+        const figure = document.createElement('figure');
+        figure.className = 'artwork';
+
+        const img = document.createElement('img');
+        img.src = painting.src;
+        img.alt = painting.alt;
+        img.onclick = function() {
+            openModal(painting.src);
+        };
+
+        const figcaption = document.createElement('figcaption');
+
+        const name = document.createElement('p');
+        name.className = 'painting-name';
+        name.innerText = painting.name;
+
+        const details = document.createElement('p');
+        details.innerText = painting.details;
+
+        figcaption.appendChild(name);
+        figcaption.appendChild(details);
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+
+        gallery.appendChild(figure);
+    });
+}
+
+fetch('assets/data/paintings.json')
+    .then(response => response.json())
+    .then(data => populateGallery(data))
+    .catch((error) => console.error('Error:', error));
