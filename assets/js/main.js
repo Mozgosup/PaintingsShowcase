9cf61f96f0f11d905fbfeb707529a943b16b3e97
mozgosup <mozgosup@gmail.com>
2023-09-25 19:16:59 +0700

    Added a view mode for photos in the artist section; refactoring JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,195 +1,194 @@
-function setActiveLanguageClass(lang) {
-    document.querySelectorAll('.lang-switcher a').forEach(a => {
-        a.classList.remove('active-lang');
-        if (a.getAttribute('lang') === lang) {
-            a.classList.add('active-lang');
-        }
-    });
-}
+(function () {
+    // Constants
+    const IMAGE_MODAL = document.getElementById('imageModal');
+    const MODAL_IMG = document.getElementById('img01');
+    const MODAL_INFO = document.getElementById('modalPaintingInfo');
+    let currentLanguage = 'en';
 
-function openModal(src, name, details) {
-    const modal = document.getElementById('imageModal');
-    const modalImage = document.getElementById('img01');
-    const modalPaintingInfo = document.getElementById('modalPaintingInfo');
+// Set active language class
+    function setActiveLanguageClass(lang) {
+        document.querySelectorAll('.lang-switcher a').forEach(a => {
+            a.classList.toggle('active-lang', a.getAttribute('lang') === lang);
+        });
+    }
 
-    modalImage.src = src;
-    modalPaintingInfo.innerHTML = `<strong>${name}</strong><br>${details}`;
+// Open modal with provided details
+    function openModal(src, name = '', details = '') {
+        MODAL_IMG.src = src;
+        MODAL_INFO.innerHTML = `<strong>${name}</strong><br>${details}`;
+        IMAGE_MODAL.style.display = "block";
+    }
 
-    modal.style.display = "block";
-}
+// Close the modal
+    function closeModal() {
+        IMAGE_MODAL.style.display = "none";
+    }
 
-function closeModal() {
-    const modal = document.getElementById('imageModal');
-    modal.style.display = "none";
-}
+// Populate gallery with provided paintings
+    function populateGallery(paintings) {
+        const gallery = document.getElementById('gallery');
+        gallery.innerHTML = '';
 
-document.getElementById('img01').addEventListener('click', function (event) {
-    event.stopPropagation();
-});
+        paintings.forEach((painting) => {
+            const figure = document.createElement('figure');
+            figure.className = 'artwork';
 
-function populateGallery(paintings) {
-    const gallery = document.getElementById('gallery');
-    gallery.innerHTML = '';
+            const img = document.createElement('img');
+            img.src = painting.src;
+            img.alt = painting.alt;
+            img.onclick = () => openModal(painting.src, painting.name[currentLanguage], painting.details);
 
-    paintings.forEach((painting) => {
-        const figure = document.createElement('figure');
-        figure.className = 'artwork';
+            const figcaption = document.createElement('figcaption');
+            figcaption.innerHTML = `<p class="painting-name">${painting.name[currentLanguage]}</p><p>${painting.details}</p>`;
 
-        const img = document.createElement('img');
-        img.src = painting.src;
-        img.alt = painting.alt;
-        img.onclick = function () {
-            openModal(painting.src, painting.name[currentLanguage], painting.details);
-        };
+            figure.append(img, figcaption);
+            gallery.appendChild(figure);
 
-        const figcaption = document.createElement('figcaption');
-
-        const name = document.createElement('p');
-        name.className = 'painting-name';
-        name.innerText = painting.name[currentLanguage];
-
-        const details = document.createElement('p');
-        details.innerText = painting.details;
-
-        figcaption.appendChild(name);
-        figcaption.appendChild(details);
-
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-
-        gallery.appendChild(figure);
-
-        img.onload = function () {
-            figcaption.style.width = img.offsetWidth + "px";
-            this.style.opacity = "1";
-        }
-    });
-}
-
-function setupObserver() {
-    let allImagesLoaded = 0;
-    const totalImages = document.querySelectorAll('#gallery img').length;
-
-    let observer = new IntersectionObserver(function (entries) {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                let img = entry.target;
-                let figcaption = img.nextElementSibling;
+            img.onload = () => {
                 figcaption.style.width = img.offsetWidth + "px";
-
-                allImagesLoaded++;
-
-                if (allImagesLoaded === totalImages) {
-                    const gallery = document.getElementById('gallery');
-                    gallery.style.visibility = "visible";
-                    gallery.style.opacity = "1";
-                }
+                img.style.opacity = "1";
             }
         });
-    });
-
-    document.querySelectorAll('#gallery img').forEach(img => {
-        observer.observe(img);
-    });
-}
-
-function changeContent(sectionId) {
-
-    const sections = document.querySelectorAll('.content-section');
-
-    sections.forEach(section => {
-        section.style.display = 'none';
-    });
-
-    document.getElementById(sectionId).style.display = 'block';
-
-    document.querySelectorAll('#menu li').forEach(item => {
-        item.classList.remove('active-menu-item');
-    });
-
-    document.getElementById(`menu-${sectionId}`).classList.add('active-menu-item');
-}
-
-function fetchJSON(url) {
-    return fetch(url).then(response => response.json());
-}
-
-document.getElementById('imageModal').addEventListener('click', function (event) {
-    if (event.target === event.currentTarget) {
-        closeModal();
     }
-});
 
+// Observer for gallery loading
+    function setupObserver() {
+        const gallery = document.getElementById('gallery');
+        let allImagesLoaded = 0;
 
-document.querySelectorAll('#menu a').forEach(link => {
-    link.addEventListener('click', function (event) {
-        event.preventDefault();
-        changeContent(this.getAttribute('data-section'));
-    });
-});
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const img = entry.target;
+                    img.nextElementSibling.style.width = img.offsetWidth + "px";
+                    allImagesLoaded++;
+                    if (allImagesLoaded === gallery.children.length) {
+                        gallery.style.visibility = "visible";
+                        gallery.style.opacity = "1";
+                    }
+                }
+            });
+        });
 
-document.querySelectorAll('.lang-switcher a').forEach(link => {
-    link.addEventListener('click', function (event) {
-        event.preventDefault();
-        switchLanguage(this.getAttribute('lang'));
-    });
-});
-
-document.addEventListener('contextmenu', function (event) {
-    if (event.target.tagName === 'IMG') {
-        event.preventDefault();
+        gallery.querySelectorAll('img').forEach(img => observer.observe(img));
     }
-});
 
-function applyTranslations(translations) {
-    const currentYear = new Date().getFullYear();
-    document.querySelectorAll('[data-translate-key]').forEach(el => {
-        const key = el.getAttribute('data-translate-key');
-        if (translations[key]) {
-            if (key === "copyrightStatement") {
-                el.textContent = translations[key].replace("{year}", currentYear);
-            } else {
-                el.textContent = translations[key];
+// Change the displayed content section
+    function changeContent(sectionId) {
+        document.querySelectorAll('.content-section').forEach(section => section.style.display = 'none');
+        document.getElementById(sectionId).style.display = 'block';
+
+        document.querySelectorAll('#menu li').forEach(item => item.classList.remove('active-menu-item'));
+        document.getElementById(`menu-${sectionId}`).classList.add('active-menu-item');
+    }
+
+// Fetch JSON data from a provided URL
+    function fetchJSON(url) {
+        return fetch(url).then(response => response.json());
+    }
+
+// Apply translation to the page
+    function applyTranslations(translations) {
+        const currentYear = new Date().getFullYear();
+        document.querySelectorAll('[data-translate-key]').forEach(el => {
+            const key = el.getAttribute('data-translate-key');
+            if (translations[key]) {
+                el.textContent = key === "copyrightStatement"
+                    ? translations[key].replace("{year}", currentYear)
+                    : translations[key];
             }
-        }
-    });
-}
-
-function loadBiography(lang) {
-    fetch(`assets/data/biography_${lang}.html`)
-        .then(response => response.text())
-        .then(data => {
-            const biographyContainer = document.getElementById('biography-container');
-            biographyContainer.innerHTML = data;
-        })
-        .catch(error => console.error('Error loading biography:', error));
-}
-
-function loadLanguage(lang) {
-    fetchJSON(`assets/data/${lang}.json`)
-        .then(data => {
-            currentLanguage = lang;
-            applyTranslations(data);
-            setActiveLanguageClass(lang);
-            return fetchJSON('assets/data/paintings.json');
-        })
-        .then(data => {
-            populateGallery(data);
-            setupObserver();
-            loadBiography(lang);
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-function switchLanguage(lang) {
-    loadLanguage(lang);
-}
-
-loadLanguage('en');
-
-document.addEventListener('keydown', function (event) {
-    if (event.key === "Escape") {
-        closeModal();
+        });
     }
-});
+
+// Load biography images and text
+    function loadBiography(lang) {
+        const biographyWrapper = document.querySelector('.image-wrapper');
+        biographyWrapper.innerHTML = ''; // Clear existing content
+
+        // Load biography text
+        fetch(`assets/data/biography_${lang}.html`)
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('biography-container').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading biography:', error));
+
+        // Load biography images
+        fetchJSON('assets/data/biography_photos.json')
+            .then(photos => {
+                photos.forEach(photo => {
+                    const img = document.createElement('img');
+                    img.src = photo.src;
+                    img.alt = photo.alt;
+                    img.className = 'biography-image';
+                    img.onclick = () => openModal(photo.src);
+                    biographyWrapper.appendChild(img);
+                });
+            })
+            .catch(error => console.error('Error loading biography photos:', error));
+    }
+
+// Load language-specific content and translations
+    function loadLanguage(lang) {
+        fetchJSON(`assets/data/${lang}.json`)
+            .then(data => {
+                currentLanguage = lang;
+                applyTranslations(data);
+                setActiveLanguageClass(lang);
+                return fetchJSON('assets/data/paintings.json');
+            })
+            .then(data => {
+                populateGallery(data);
+                setupObserver();
+                loadBiography(lang);
+            })
+            .catch(error => console.error('Error:', error));
+    }
+
+    // Event Handlers
+    function handleMenuClick(event) {
+        event.preventDefault();
+        if (event.target.tagName === 'A') {
+            changeContent(event.target.getAttribute('data-section'));
+        }
+    }
+
+    function handleLangSwitcherClick(event) {
+        event.preventDefault();
+        if (event.target.tagName === 'A') {
+            loadLanguage(event.target.getAttribute('lang'));
+        }
+    }
+
+    function handleDocumentKeydown(event) {
+        if (event.key === "Escape") {
+            closeModal();
+        }
+    }
+
+    function handleContextMenu(event) {
+        if (event.target.tagName === 'IMG') {
+            event.preventDefault();
+        }
+    }
+
+// Initial setup
+    document.addEventListener('DOMContentLoaded', () => {
+        // Load default language
+        loadLanguage(currentLanguage);
+
+        // Event listeners
+        document.querySelector('.close').addEventListener('click', closeModal);
+        IMAGE_MODAL.addEventListener('click', (event) => {
+            if (event.target === IMAGE_MODAL || event.target.classList.contains('close')) {
+                closeModal();
+            }
+        });
+        MODAL_IMG.addEventListener('click', (event) => event.stopPropagation());
+        document.getElementById('menu').addEventListener('click', handleMenuClick);
+        document.querySelector('.lang-switcher').addEventListener('click', handleLangSwitcherClick);
+        document.addEventListener('keydown', handleDocumentKeydown);
+        document.addEventListener('contextmenu', handleContextMenu);
+    });
+})();
 
